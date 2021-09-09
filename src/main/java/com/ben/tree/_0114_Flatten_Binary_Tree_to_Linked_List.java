3c58720f743bcb54e5bf1206b0785b4804737453
Benjamin <benjamin.liu@live.cn>
2021-09-08 20:05:29 -0400

    tree, 114 flatten binary tree to linked list 116 populating next right pointers in each node 226 invert binary tree

---
+++ src/main/java/com/ben/tree/_0114_Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,97 @@
+package com.ben.tree;
+
+import com.ben.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _0114_Flatten_Binary_Tree_to_Linked_List {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        flatten(root);
+    }
+
+    static public void flatten(TreeNode root) {
+        flatten(root, null);
+    }
+
+    /**
+     * Need more care
+     *
+     * @param root
+     * @param firstNode
+     * @return
+     */
+    static public TreeNode flatten(TreeNode root, TreeNode firstNode) {
+        if (root == null) {
+            return firstNode;
+        }
+
+        //Flatten right branch, return firstNode
+        firstNode = flatten(root.right, firstNode);
+        //Flatten left branch, and add right branch to the end of left branch, and return firstNode
+        firstNode = flatten(root.left, firstNode);
+
+        //Add the firstNode to the root's right node
+        root.right = firstNode;
+        root.left = null;
+
+        return root;
+    }
+
+    static public void flatten_2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        flatten_2(root.left);
+        flatten_2(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode cur = root;
+        while (cur.right != null) {
+            cur = cur.right;
+        }
+
+        cur.right = right;
+    }
+
+    static public void flatten_1(TreeNode root) {
+        List<TreeNode> allNode = new ArrayList<>();
+        addNode(root, allNode);
+
+        for (int i = 1; i < allNode.size(); i++) {
+            TreeNode pre = allNode.get(i - 1);
+            TreeNode cur = allNode.get(i);
+            pre.left = null;
+            pre.right = cur;
+        }
+    }
+
+    static public void addNode(TreeNode root, List<TreeNode> allNode) {
+        if (root == null) {
+            return;
+        }
+        allNode.add(root);
+
+        if (root.left != null) {
+            addNode(root.left, allNode);
+        }
+        if (root.right != null) {
+            addNode(root.right, allNode);
+        }
+    }
+}
