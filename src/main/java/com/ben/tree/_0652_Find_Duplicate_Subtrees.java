5d75e4cf295273f893c4f53d487140b21a053537
Benjamin <benjamin.liu@live.cn>
2021-09-14 19:53:13 -0400

    tree 652 find duplicate subtrees

---
+++ src/main/java/com/ben/tree/_0652_Find_Duplicate_Subtrees.java
@@ -0,0 +1,45 @@
+package com.ben.tree;
+
+import com.ben.common.TreeNode;
+import com.ben.util.TreeUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class _0652_Find_Duplicate_Subtrees {
+    public static void main(String[] args) {
+
+    }
+
+    static HashMap<String, Integer> memo;
+    static List<TreeNode> res;
+
+    static public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        memo = new HashMap<>();
+        res = new ArrayList<>();
+
+        traverse(root);
+
+        return res;
+    }
+
+    static public String traverse(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+
+        String left = traverse(root.left);
+        String right = traverse(root.right);
+
+        String cur = left + "," + right + "," + root.val;
+
+        int count = memo.getOrDefault(cur, 0);
+        if (count == 1) {
+            res.add(root);
+        }
+        memo.put(cur, count + 1);
+
+        return cur;
+    }
+}
