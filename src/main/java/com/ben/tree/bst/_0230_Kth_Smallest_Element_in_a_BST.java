1c0f1ea7c4902b9ccab61211949ea748d948c0c3
ashey <sheye@huali-tec.com>
2021-11-15 20:16:00 -0500

    convert bst to greater tree

--- src/main/java/com/ben/tree/bst/_0230_Kth_Smallest_Element_in_a_BST.java
+++ src/main/java/com/ben/tree/bst/_0230_Kth_Smallest_Element_in_a_BST.java
@@ -1,18 +1,56 @@
 package com.ben.tree.bst;
 
 import com.ben.common.TreeNode;
+import com.ben.util.PrintUtil;
+import com.ben.util.TreeUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class _0230_Kth_Smallest_Element_in_a_BST {
 
     public static void main(String[] args) {
+        TreeNode root = TreeUtil.createTree(5,3,6,2,4,null,null,1);
 
+        int val = kthSmallest(root, 3);
+        System.out.println(val);
     }
+
+    private static Map<Integer, Integer> subtreeCountMap;
+
+    static public int kthSmallest(TreeNode root, int k) {
+
+        subtreeCountMap = new HashMap<>();
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        }
+        count++;
+        if (k == count) {
+            return root.val;
+        }
+
+        return kthSmallest(root.right, k - count);
+    }
+
+    static private int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Integer count = subtreeCountMap.get(root.val);
+        if (count == null) {
+            count = 1 + countNodes(root.left) + countNodes(root.right);
+            subtreeCountMap.put(root.val, count);
+        }
+        return count;
+    }
+
 
     private static int val;
     private static int rank;
     private static boolean found;
 
-    static public int kthSmallest(TreeNode root, int k) {
+    static public int kthSmallest1(TreeNode root, int k) {
         rank = 0;
         found = false;
         traverse(root, k);
---
+++ src/main/java/com/ben/tree/bst/_0538_Convert_BST_to_Greater_Tree.java
@@ -0,0 +1,37 @@
+package com.ben.tree.bst;
+
+import com.ben.common.TreeNode;
+
+public class _0538_Convert_BST_to_Greater_Tree {
+
+    public static void main(String[] args) {
+
+    }
+
+    static private int sum;
+
+    static public TreeNode convertBST(TreeNode root) {
+        sum = 0;
+
+        dfs(root);
+
+        return root;
+    }
+
+    private static void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.right != null) {
+            dfs(root.right);
+        }
+
+        sum += root.val;
+        root.val = sum;
+
+        if (root.left != null) {
+            dfs(root.left);
+        }
+    }
+}
