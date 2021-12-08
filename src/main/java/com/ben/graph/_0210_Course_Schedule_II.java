c3ac8799a3a83840c271ba12e1e0667ebc2e4522
ashey <sheye@huali-tec.com>
2021-12-07 20:51:06 -0500

    Course schedule 2

--- src/main/java/com/ben/graph/_0210_Course_Schedule_II.java
+++ src/main/java/com/ben/graph/_0210_Course_Schedule_II.java
@@ -2,14 +2,12 @@ package com.ben.graph;
 
 import com.ben.util.PrintUtil;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class _0210_Course_Schedule_II {
 
     public static void main(String[] args) {
-        PrintUtil.printArray(new Solution1().findOrder(2, new int[][]{{0, 1}}));
+        PrintUtil.printArray(new Solution2().findOrder(2, new int[][]{{0, 1}}));
     }
 
     public static class Solution1 {
@@ -104,6 +102,55 @@ public class _0210_Course_Schedule_II {
                 graph[from].add(to);
             }
             return graph;
+        }
+    }
+
+    public static class Solution2 {
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            int[] prerequisitesCount = new int[numCourses];
+            int[] res = new int[numCourses];
+
+            List<Integer>[] graph = new ArrayList[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < prerequisites.length; i++) {
+                int pre = prerequisites[i][1];
+                int cur = prerequisites[i][0];
+                prerequisitesCount[cur]++;
+                graph[pre].add(cur);
+            }
+
+            int finishedCourseCount = 0;
+            Queue<Integer> finishedCourse = new LinkedList<>();
+            for (int i = 0; i < numCourses; i++) {
+                //those course does not have prerequisites can be finished first
+                if (prerequisitesCount[i] == 0) {
+                    finishedCourse.offer(i);
+                    res[finishedCourseCount++] = i;
+                }
+            }
+
+            while (!finishedCourse.isEmpty()) {
+                //finished course
+                int aFinishedCourse = finishedCourse.poll();
+                for (int to : graph[aFinishedCourse]) {
+                    //this course has a finished prerequisites, so we decrease its prerequisites count by 1
+                    prerequisitesCount[to]--;
+                    //if this course's all prerequisites have been finished, we can finished this course
+                    if (prerequisitesCount[to] == 0) {
+                        finishedCourse.add(to);
+                        res[finishedCourseCount++] = to;
+                    }
+                }
+            }
+
+            if (finishedCourseCount == numCourses) {
+                return res;
+            }
+
+            return new int[0];
         }
     }
 }
