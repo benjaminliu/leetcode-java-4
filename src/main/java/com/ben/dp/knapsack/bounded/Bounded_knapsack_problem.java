742ca4e92a0547674f4dd33e57d96764d1e526f2
ashley <benjamin.liu@live.cn>
2022-08-02 18:31:07 -0400

    dp

---
+++ src/main/java/com/ben/dp/knapsack/bounded/Bounded_knapsack_problem.java
@@ -0,0 +1,6 @@
+package com.ben.dp.knapsack.bounded;
+
+public class Bounded_knapsack_problem {
+    //多重背包， 可以转换成 0-1背包
+    //https://github.com/youngyangyang04/leetcode-master/blob/master/problems/%E8%83%8C%E5%8C%85%E9%97%AE%E9%A2%98%E7%90%86%E8%AE%BA%E5%9F%BA%E7%A1%80%E5%A4%9A%E9%87%8D%E8%83%8C%E5%8C%85.md
+}
