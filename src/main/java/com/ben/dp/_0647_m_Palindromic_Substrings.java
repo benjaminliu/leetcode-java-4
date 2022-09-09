c5faa9c79af3c653c5a7353860ebc08d49826121
ashley <benjamin.liu@live.cn>
2022-09-09 19:22:19 -0400

    dp

---
+++ src/main/java/com/ben/dp/_0647_m_Palindromic_Substrings.java
@@ -0,0 +1,39 @@
+package com.ben.dp;
+
+public class _0647_m_Palindromic_Substrings {
+
+    class Solution {
+        public int countSubstrings(String s) {
+            boolean[][] dp = new boolean[s.length()][s.length()];
+
+            for (int right = 0; right < s.length(); right++) {
+                for (int left = 0; left <= right; left++) {
+                    if (s.charAt(right) == s.charAt(left)) {
+                        // one letter is palindromic
+                        // two letter, because s.charAt(i) == s.charAt(j), is also palindromic
+                        if (right - left < 3) {
+                            dp[left][right] = true;
+                        } else {
+                            //remove left letter and right letter, to see if it is palindromic
+                            //such as  "abccba",  remove left and right letter, become "bccb"
+                            dp[left][right] = dp[left + 1][right - 1];
+                        }
+                    } else {
+                        //Not equal, cannot be palindromic
+                        dp[left][right] = false;
+                    }
+                }
+            }
+
+            int res = 0;
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = 0; j < s.length(); j++) {
+                    if (dp[i][j]) {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
