350c1c8f43a299e2a74222f30d61f9e55ecd0314
ashley <benjamin.liu@live.cn>
2022-09-25 18:24:19 -0400

    Array

--- src/main/java/com/ben/array/_0189_m_Rotate_Array.java
+++ src/main/java/com/ben/array/_0189_m_Rotate_Array.java
@@ -39,4 +39,27 @@ public class _0189_m_Rotate_Array {
             nums[right] = temp;
         }
     }
+
+    class Solution1 {
+        public void rotate(int[] nums, int k) {
+            k = k % nums.length;
+            reverse(nums, 0, nums.length - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, nums.length - 1);
+        }
+
+        void reverse(int[] nums, int left, int right) {
+            for (int i = left, j = right; i < j; i++, j--) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        void swap(int[] nums, int left, int right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+    }
 }
